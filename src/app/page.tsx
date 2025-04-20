d1ad2ca152768d9eed39f22802efb9a7176bb166
Anderson R. <andersonrespla@gmail.com>
2025-04-19 22:41:48 -0300

    feat: integra Header, Sidebar e Dashboard na home

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Dashboard from "../components/Dashboard";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 overflow-auto p-4 bg-gray-50">
+          <Dashboard />
+        </main>
+      </div>
+    </div>
+  );
+}
